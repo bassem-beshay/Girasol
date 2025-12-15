65b1b98cdb0ab3265dd6de46202b76790e291bea
bassem-beshay <bassembeshay50@gmail.com>
2025-12-15 03:10:22 -0800

    Add Passenger Python configuration

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+#!/home4/girasolt/girasol/backend/venv/bin/python3
+import os
+import sys
+
+# Add the backend directory to the path
+sys.path.insert(0, '/home4/girasolt/girasol/backend')
+sys.path.insert(0, '/home4/girasolt/girasol/backend/venv/lib/python3.9/site-packages')
+
+# Set the settings module
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.shared_hosting'
+
+# Import the WSGI application
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
